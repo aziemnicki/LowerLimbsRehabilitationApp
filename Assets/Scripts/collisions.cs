2c0ded1c82657a2993226712e9097746a2119fd1
aziemnicki <126728897+aziemnicki@users.noreply.github.com>
2024-08-29 01:25:28 +0200

    Update collisions.cs

--- Assets/Scripts/collisions.cs
+++ Assets/Scripts/collisions.cs
@@ -4,81 +4,76 @@ using UnityEngine;
 
 public class collisions : MonoBehaviour
 {
-    // Start is called before the first frame update
-      private Detection detectionScript;
-      private int counter_left = 0;
-      private int counter_right = 0;
+   // Start is called before the first frame update
+   private Detection detectionScript;
+   private int counter_left = 0;
+   private int counter_right = 0;
    private bool trigger_delete = false;
-    void Start()
-    {
-      detectionScript = FindAnyObjectByType<Detection>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+   void Start()
+   {
+   detectionScript = FindAnyObjectByType<Detection>();
+   }
 
-     void OnTriggerEnter(Collider collision)
-     {
-         if (detectionScript != null)
+   void OnTriggerEnter(Collider collision)
+   {
+      if (detectionScript != null)
+      {
+         if (collision.CompareTag("left_step"))
          {
-            if (collision.CompareTag("left_step"))
-            {
-               detectionScript.SetIsCollision(true);
-               detectionScript.GetTriggeringObjectName(collision.gameObject.name);
-               // Debug.Log("OOOOOOOOOOOOOOOOOOOOO  Entered collision with " + collision.gameObject.name);
-            }
-            else if (collision.CompareTag("right_step"))
-            {
-               detectionScript.SetIsCollision(true);
-               detectionScript.GetTriggeringObjectName(collision.gameObject.name);
-               // Debug.Log("LLLLLLLLLLLLLLLLLLLLLL  Entered collision with " + collision.gameObject.name);
-            }
+            detectionScript.SetIsCollision(true);
+            detectionScript.GetTriggeringObjectName(collision.gameObject.name);
+            // Debug.Log("OOOOOOOOOOOOOOOOOOOOO  Entered collision with " + collision.gameObject.name);
          }
-     }
-
-     void OnTriggerStay(Collider collision)
-     {
-         if (detectionScript != null)
+         else if (collision.CompareTag("right_step"))
          {
-
-            if (collision.CompareTag("left_step"))
-            {
-               detectionScript.SetIsCollision(false);
-               counter_left += 1;
-            }
-            else if (collision.CompareTag("right_step"))
-            {
-               detectionScript.SetIsCollision(false);
-               counter_right += 1;
-            }
-
-            if ((collision.CompareTag("left_step") && counter_left >= 10) || (collision.CompareTag("right_step") && counter_right >= 10))
-            {
-               trigger_delete = true;  
-            }
-            
-            if(trigger_delete)
-            {
-               detectionScript.SetIsCollision(false);
-               detectionScript.DeleteOldestStep();
-               counter_left = 0;
-               counter_right = 0;
-               trigger_delete = false;
-            }
+            detectionScript.SetIsCollision(true);
+            detectionScript.GetTriggeringObjectName(collision.gameObject.name);
+            // Debug.Log("LLLLLLLLLLLLLLLLLLLLLL  Entered collision with " + collision.gameObject.name);
          }
-     }
+      }
+   }
 
-     void OnTriggerExit(Collider collision)
-     {
-         if (detectionScript != null)
+   void OnTriggerStay(Collider collision)
+   {
+      if (detectionScript != null)
+      {
+         if (collision.CompareTag("left_step"))
          {
             detectionScript.SetIsCollision(false);
+            counter_left += 1;
+         }
+         else if (collision.CompareTag("right_step"))
+         {
+            detectionScript.SetIsCollision(false);
+            counter_right += 1;
+         }
+
+         if ((collision.CompareTag("left_step") && counter_left >= 10) || (collision.CompareTag("right_step") && counter_right >= 10))
+         {
+            trigger_delete = true;  
+         }
+         
+         if(trigger_delete)
+         {
+            detectionScript.SetIsCollision(false);
+            detectionScript.DeleteOldestStep();
             counter_left = 0;
             counter_right = 0;
+            trigger_delete = false;
          }
-     }
+      }
+   }
+
+   void OnTriggerExit(Collider collision)
+   {
+      if (detectionScript != null)
+      {
+         detectionScript.SetIsCollision(false);
+         counter_left = 0;
+         counter_right = 0;
+      }
+   }
 }
 
